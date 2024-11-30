61e86f4c6223fa9d30f3bd8a7fd022a45b776cdf
Jeremy GULLIET <jrmy.gulliet@gmail.com>
2024-11-30 11:44:24 +0100

    Ajout google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Karla } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Script from "next/script";
 
 const karla = Karla({
   subsets: ["latin"],
@@ -116,11 +117,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MN76QMBG');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${karla.className} antialiased`}>
-        {/* Google Tag Manager (noscript) */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MN76QMBG"
-height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
-{/* End Google Tag Manager (noscript) */}
         <div className="flex min-h-screen flex-col">
           <Header />
           <main className="flex-1 bg-gradient-to-r from-espace to-black pt-24">
